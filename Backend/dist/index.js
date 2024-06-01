02872b0f6ff8952157780233e4d0292c5d5b4b41
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2024-06-01 16:17:19 +0530

    websockets backend initialised

---
+++ Backend/dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const app = (0, express_1.default)();
+const httpServer = app.listen(8080, () => {
+    console.log("listening to port 8080");
+});
+app.get("/", (req, res) => {
+    res.send("hi there");
+});
+const wss = new ws_1.WebSocketServer({ server: httpServer });
+let user = 0;
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === ws_1.WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    console.log("user", ++user);
+    ws.send('Hello! Message From Server!!');
+});
---
+++ Backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import { WebSocketServer,WebSocket } from 'ws'
+
+const app = express()
+const httpServer = app.listen(8080,()=>{
+    console.log("listening to port 8080");
+})
+app.get("/",(req,res)=>{
+    res.send("hi there");
+})
+
+const wss = new WebSocketServer({ server: httpServer });
+let user=0;
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+  
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+  console.log("user",++user);
+
+
+  ws.send('Hello! Message From Server!!');
+});
